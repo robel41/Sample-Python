0bbef4a1bfda1cfe0bd2b544b089554e02a13cd7
Robel <tamratdebebe41@gmail.com>
2023-07-20 11:31:57 -0400

    Create itunes.py

---
+++ itunes.py
@@ -0,0 +1,16 @@
+import requests
+import sys
+import json
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=14&term=" + sys.argv[1])
+#print(json.dumps(response.json(), indent =2))
+
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])
+
+
+
